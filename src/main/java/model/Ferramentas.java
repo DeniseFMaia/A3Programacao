e404d20bcd71b3354104eee10f765aad2941e282
denise <denise@10.150.111.156>
2024-05-13 19:58:33 -0300

    test

--- src/main/java/model/Ferramentas.java
+++ src/main/java/model/Ferramentas.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author denise
- */
 public class Ferramentas {
-    
+    // Atributos
+    public String Nome;
+    public String Marca;
+    public Double Custo;
+
+    // Construtor Default
+    public Ferramentas(){
+        this("", "", 0.0);
+    }
+    // Construtor 
+    public Ferramentas(String Nome, String Marca, Double Custo){
+        this.Nome = Nome;
+        this.Marca = Marca;
+        this.Custo = Custo;
+    }
+
+    // Getters 
+    public String getNome(){
+        return this.Nome;
+    }
+    public String getMarca(){
+        return this.Marca;
+    }
+    public Double getCusto(){
+        return this.Custo;
+    }
+
+    // Setters
+    private void setNome(String nome){
+        this.Nome = nome;
+    }
+    private void setMarca(String marca){
+        this.Marca = marca;
+    }
+    private void setCusto(Double custo){
+        this.Custo = custo;
+    }
+
+    // Metodos a serem implementados
 }
--- src/main/java/visao/Mensagens.java
+++ src/main/java/visao/Mensagens.java
@@ -2,8 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package principal;
+package visao;
 
-public class Principal {
+/**
+ *
+ * @author denise
+ */
+public class Mensagens {
     
 }
