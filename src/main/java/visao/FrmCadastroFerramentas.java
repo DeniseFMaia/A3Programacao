2a0bc504267f45309a8b4950b333110671a4260e
10724112695 <10724112695@UNS11DT099820.animaedu.intranet>
2024-05-20 20:53:33 -0300

    EDIT 2: Tela Cadastro Ferramenta

--- src/main/java/visao/FrmCadastroFerramentas.java
+++ src/main/java/visao/FrmCadastroFerramentas.java
@@ -26,51 +26,51 @@ public class FrmCadastroFerramentas extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        jLnomeFerramenta = new javax.swing.JLabel();
+        JTFnomeFerramenta = new javax.swing.JTextField();
+        jLmarca = new javax.swing.JLabel();
+        JTFmarca = new javax.swing.JTextField();
+        jLcustoAquisição = new javax.swing.JLabel();
+        JTFcustoAquisição = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        jTextCadastroFerramenta = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jTextCadastreSuaFerramenta = new javax.swing.JTextArea();
+        b_registrarFerramenta = new javax.swing.JButton();
+        b_excluir = new javax.swing.JButton();
+        b_gerenciarFerramenta = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Nome da Ferramenta:");
+        jLnomeFerramenta.setText("Nome da Ferramenta:");
 
-        jLabel2.setText("Marca:");
-
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+        JTFnomeFerramenta.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+                JTFnomeFerramentaActionPerformed(evt);
             }
         });
 
-        jLabel3.setText("Custo de Aquisição:");
+        jLmarca.setText("Marca:");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jTextArea1.setRows(5);
-        jTextArea1.setText("     \n     Cadastro Ferramentas");
-        jScrollPane1.setViewportView(jTextArea1);
+        jLcustoAquisição.setText("Custo de Aquisição:");
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        jTextArea2.setRows(5);
-        jTextArea2.setText("\n\n\n\nCADASTRE SUA\n FERRAMENTA!");
-        jScrollPane2.setViewportView(jTextArea2);
+        jTextCadastroFerramenta.setColumns(20);
+        jTextCadastroFerramenta.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jTextCadastroFerramenta.setRows(5);
+        jTextCadastroFerramenta.setText("     \n     Cadastro Ferramentas");
+        jScrollPane1.setViewportView(jTextCadastroFerramenta);
 
-        jButton1.setText("Registrar Ferramenta");
+        jTextCadastreSuaFerramenta.setColumns(20);
+        jTextCadastreSuaFerramenta.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jTextCadastreSuaFerramenta.setRows(5);
+        jTextCadastreSuaFerramenta.setText("\n\n\n\nCADASTRE SUA\n FERRAMENTA!");
+        jScrollPane2.setViewportView(jTextCadastreSuaFerramenta);
 
-        jButton2.setText("Excluir");
+        b_registrarFerramenta.setText("Registrar Ferramenta");
 
-        jButton3.setText("Gerenciar Ferramenta");
+        b_excluir.setText("Excluir");
+
+        b_gerenciarFerramenta.setText("Gerenciar Ferramenta");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,23 +82,23 @@ public class FrmCadastroFerramentas extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel1))
+                            .addComponent(JTFcustoAquisição, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLcustoAquisição)
+                            .addComponent(JTFmarca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(JTFnomeFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLmarca)
+                            .addComponent(jLnomeFerramenta))
                         .addGap(64, 64, 64))
                     .addGroup(layout.createSequentialGroup()
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(b_registrarFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(b_excluir, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(b_gerenciarFerramenta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addContainerGap())))))
         );
         layout.setVerticalGroup(
@@ -108,24 +108,24 @@ public class FrmCadastroFerramentas extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel1)
+                        .addComponent(jLnomeFerramenta)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(JTFnomeFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(8, 8, 8)
-                        .addComponent(jLabel2)
+                        .addComponent(jLmarca)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(JTFmarca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jLabel3)
+                        .addComponent(jLcustoAquisição)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(JTFcustoAquisição, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(27, 27, 27)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jButton1)
-                            .addComponent(jButton3))
+                            .addComponent(b_registrarFerramenta)
+                            .addComponent(b_gerenciarFerramenta))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton2)
-                        .addGap(0, 32, Short.MAX_VALUE))
+                        .addComponent(b_excluir)
+                        .addGap(0, 30, Short.MAX_VALUE))
                     .addComponent(jScrollPane2))
                 .addContainerGap())
         );
@@ -133,9 +133,9 @@ public class FrmCadastroFerramentas extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+    private void JTFnomeFerramentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFnomeFerramentaActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    }//GEN-LAST:event_JTFnomeFerramentaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -173,18 +173,18 @@ public class FrmCadastroFerramentas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField JTFcustoAquisição;
+    private javax.swing.JTextField JTFmarca;
+    private javax.swing.JTextField JTFnomeFerramenta;
+    private javax.swing.JButton b_excluir;
+    private javax.swing.JButton b_gerenciarFerramenta;
+    private javax.swing.JButton b_registrarFerramenta;
+    private javax.swing.JLabel jLcustoAquisição;
+    private javax.swing.JLabel jLmarca;
+    private javax.swing.JLabel jLnomeFerramenta;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextArea jTextCadastreSuaFerramenta;
+    private javax.swing.JTextArea jTextCadastroFerramenta;
     // End of variables declaration//GEN-END:variables
 }
